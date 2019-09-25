84713ff76af1c93968c17e0280791ba7c1d4fc18
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-09-25 12:09:10 +0700

    update manager

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -12,6 +12,25 @@ class MenuController {
     })
   }
 
+  static create(req, res) {
+    Menu.create({where : {
+      menu_name : req.body.menu_name,
+      price: req.body.price
+    }})
+    .then(newMenu => {
+      res.redirect('/')
+    })
+    .catch(err => {
+      let url = ''
+      err.errors.forEach((error, index) => {
+        url += `err${index}=${error.message}&`
+      })
+      res.redirect(`/menus/add?${url}` )
+    })
+  }
+
+  static edit ()
+
 }
 
 module.exports = MenuController;
--- routers/menuRouter.js
+++ routers/menuRouter.js
@@ -1,6 +1,33 @@
 const routes = require('express').Router();
 const menuController = require('../controllers/menuController');
 
+//HOME
 routes.get('/', menuController.allMenu)
+
+
+//=================CRUD MENU=============//
+
+
+//create
+routes.get('/menus/add', (req, res) => {
+    let errors = []
+
+    for (const key in req.query) {
+        errors.push(req.query[key])
+    }
+    res.render('newMenu', {errors})
+})
+routes.post('/menus/add', menuController.create)
+
+
+//edit & update
+routes.get('/menus/edit/:id', menuController.edit)
+routes.post('/menus/edit/:id', menuController.update)
+
+
+//delete
+routes.get('/menus/delete:id', menuController.delete)
+
+
 
 module.exports = routes;
